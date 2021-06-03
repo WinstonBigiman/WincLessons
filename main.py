fefdc26f37d4fd080394754d38292517d854f4f2
WinstonBigiman <82651092+WinstonBigiman@users.noreply.github.com>
2021-06-03 14:33:41 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 # Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
+__winc_id__ = '25596924dffe436da9034d43d0af6791'
+__human_name__ = 'conditions'
 
 # Add your code after this line
-# Part 1
-example_1 = "Ruud Gullit"
-example_2 = "Marco van Basten"
-goal_0 = 32
-goal_1 = 54
-scorers = example_1 + " " + str(goal_0) + ", " + example_2 + " " + str(goal_1)
-print (scorers)
-#report = ((example_1) + "scored in the " + str(goal_0) + "nd minute, " + (example_2) + "scored in the " + str(goal_1) + "th minute")
-report = example_1 + " scored in the " + str(goal_0) + "nd minute\n" + example_2 + " scored in the " + str(goal_1) + "th minute"
-print (report)
-# Part 2
-player = "Gerald Vanenburg"
-# a = player.find(" "[:])
-first_name = player[0:(player.find(" "))]
-#print (first_name)
-# 2.3
-last_name_len = len(player[(player.find(" "))+1:])
-#print (last_name_len)
-# 2.4
-name_short = f'{player[0]}. {player[player.find(" ")+1:]}'
-print(name_short)
-# 2.5
-chant = f'{player[0:player.find(" ")]}! '*player.find(" ")
-chant = chant.rstrip()
-print (chant)
-good_chant = chant[-1] != " "
-print (good_chant)
+def farm_action(weather, tod, cowmilk_status, location_of_cows, season, slurry_tank, grass_status):
+        if (weather == "rainy" and location_of_cows == "pasture") or (tod == "night" and location_of_cows == "pasture"):
+            print("take cows to cowshed")
+            return "take cows to cowshed" 
+        
+        if cowmilk_status == True and location_of_cows == "cowshed":
+            print("milk cows")
+            return "milk cows"
+
+        elif cowmilk_status == True and location_of_cows == "pasture":
+            #print("""Take cows tot cowshed\nMilk cows\nBring cows back to pasture""")
+            return """Take cows tot cowshed\nMilk cows\nTake cows back to pasture""" 
+            
+        
+        elif slurry_tank == True and location_of_cows == "cowshed" and weather != "sunny" and weather != "windy":
+            print("fertilize pasture")
+            return "fertilize pasture"
+        elif slurry_tank == True and location_of_cows == "pasture" and weather != "sunny" and weather != "windy":
+            print("""Bring cows tot cowshed\nFertilize pasture\nBring cows back to pasture""")
+            return """Bring cows tot cowshed\nFertilize pasture\nBring cows back to pasture"""
+        elif grass_status == True and season == "spring" and weather == "sunny" and location_of_cows == "cowshed":
+            print("mow grass")
+            return "mow grass"
+        elif grass_status == True and season == "spring" and weather == "sunny" and location_of_cows == "pasture":
+            print("""Bring cows tot cowshed\nMow grass\nBring cows back to pasture""")
+            return """Bring cows tot cowshed
+                    Mow grass
+                    Bring cows back to pasture"""
+
+        else: 
+            print("wait")
+            return "wait"
 
 
-
+print(farm_action("sunny", "day", True, "pasture", "spring", False, True))
